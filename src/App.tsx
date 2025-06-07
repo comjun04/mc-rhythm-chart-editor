4447f8a3b595922ed590c61f84a9b12dba1358a3
comjun04 <comjun04@gmail.com>
2025-06-07 17:59:49 +0900

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <h1 className="text-3xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="relative">
+      <nav className="fixed top-0 h-12 w-full bg-neutral-900">
+        <h2 className="text-xl">마크리겜채보짜기툴</h2>
+      </nav>
+    </div>
   )
 }
 
